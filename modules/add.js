1dc6ab2fac79b9e8f0579c8abe90c55c61a3ebfd
Bilal Alsilwadi <balsilwadi@gmail.com>
2023-06-09 15:17:08 -0500

    intro to node.js

---
+++ modules/add.js
@@ -0,0 +1,7 @@
+const sum = (a,b) => {
+    return a+b;
+};
+console.log(sum(1,1))
+
+
+module.exports = sum;// allows us to import the sum function in other files
---
+++ modules/index.js
@@ -0,0 +1,6 @@
+//To run this file:
+//  1. Enter your terminal and navigate to helloworld file (cd modules). 
+//  2. Then run command "node index.js"
+
+
+require('./add.js')//runs module from add.js
